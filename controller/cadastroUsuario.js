a796f6e5a253e9d6c9e4d0e4b506d31632ad23af
Aroldo12 <laroldo206@gmail.com>
2025-06-24 21:55:37 -0300

    Atualização semanal do gta5 online

--- controller/cadastroUsuario.js
+++ controller/cadastroUsuario.js
@@ -2,63 +2,84 @@ import jwt from 'jsonwebtoken';
 import { Usuarios } from '../models/usuarios.js';
 
 export const cadastrarUsuario = async (req, res) => {
-    const { nome, email, senha, telefone, tipo_usuario} = req.body;
+    const { nome, email, senha, telefone, tipo_usuario } = req.body;
 
-        const usuarioExistente = await Usuarios.findOne({ where: {email} })
-        if (usuarioExistente) {
-            return res.status(400).json({ mensagem: 'Usuario existente'})
-        }
-        const novo = await Usuarios.create({
-            nome,
-            email,
-            senha,
-            telefone,
-            tipo_usuario
-        })
-        res.status(201).json(novo)
+    const usuarioExistente = await Usuarios.findOne({ where: { email } })
+    if (usuarioExistente) {
+        return res.status(400).json({ mensagem: 'Usuario existente' })
     }
-    export const login = async (req, res) => {
-        const { email, senha } = req.body;
-        const usuario = await Usuarios.findOne({ where: { email, senha }});
+    const novo = await Usuarios.create({
+        nome,
+        email,
+        senha,
+        telefone,
+        tipo_usuario
+    })
+    res.status(201).json(novo)
+}
+export const login = async (req, res) => {
+    const { email, senha } = req.body;
+    const usuario = await Usuarios.findOne({ where: { email, senha } });
+    if (!usuario) {
+        return res.status(401).json({ mensagem: 'Usuário ou senha inválidos' });
+    }
+    const token = jwt.sign(
+        { id: usuario.id_usuario, tipo_usuario: usuario.tipo_usuario }, // Inclui o tipo_usuario no token
+        process.env.SEGREDO_JWT,
+        { expiresIn: '1h' }
+    );
+    res.json({ token, tipo_usuario: usuario.tipo_usuario }); // Retorna o tipo_usuario na resposta
+};
+export const atualizarUsuario = async (req, res) => {
+    const { id } = req.params; // ID do usuário a ser atualizado
+    const { nome, email, senha, telefone, tipo_usuario } = req.body;
+
+    try {
+        const usuario = await Usuarios.findByPk(id);
         if (!usuario) {
-            return res.status(401).json({ mensagem: 'Usuario ou senha invalidos'})
+            return res.status(404).json({ mensagem: 'Usuário não encontrado' });
         }
-        const token = jwt.sign({ id: usuario.id_usuario }, process.env.SEGREDO_JWT, {expiresIn: '1h'})
-        res.json({ token })
+
+        await usuario.update({ nome, email, senha, telefone, tipo_usuario });
+
+        return res.status(200).json({ mensagem: 'Usuário atualizado com sucesso', usuario });
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro ao atualizar usuário', erro: error.message });
     }
-    export const atualizarUsuario = async (req, res) => {
-        const { id } = req.params; // ID do usuário a ser atualizado
-        const { nome, email, senha, telefone, tipo_usuario } = req.body;
-    
-        try {
-            const usuario = await Usuarios.findByPk(id);
-            if (!usuario) {
-                return res.status(404).json({ mensagem: 'Usuário não encontrado' });
-            }
-    
-            await usuario.update({ nome, email, senha, telefone, tipo_usuario });
-    
-            return res.status(200).json({ mensagem: 'Usuário atualizado com sucesso', usuario });
-        } catch (error) {
-            return res.status(500).json({ mensagem: 'Erro ao atualizar usuário', erro: error.message });
+}
+
+// Excluir usuário
+export const excluirUsuario = async (req, res) => {
+    const { id } = req.params; // ID do usuário a ser excluído
+
+    try {
+        const usuario = await Usuarios.findByPk(id);
+        if (!usuario) {
+            return res.status(404).json({ mensagem: 'Usuário não encontrado' });
         }
-    };
-    
-    // Excluir usuário
-    export const excluirUsuario = async (req, res) => {
-        const { id } = req.params; // ID do usuário a ser excluído
-    
-        try {
-            const usuario = await Usuarios.findByPk(id);
-            if (!usuario) {
-                return res.status(404).json({ mensagem: 'Usuário não encontrado' });
-            }
-    
-            await usuario.destroy();
-    
-            return res.status(200).json({ mensagem: 'Usuário excluído com sucesso' });
-        } catch (error) {
-            return res.status(500).json({ mensagem: 'Erro ao excluir usuário', erro: error.message });
-        }
-    };
+
+        await usuario.destroy();
+
+        return res.status(200).json({ mensagem: 'Usuário excluído com sucesso' });
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro ao excluir usuário', erro: error.message });
+    }
+}
+export const visualizarUsuarios = async (req, res) => {
+    try {
+        const usuarios = await Usuarios.findAll({
+            attributes: ['id_usuario', 'nome', 'email', 'telefone', 'tipo_usuario'],
+            order: [['nome', 'ASC']]
+        });
+        return res.status(200).json({ mensagem: 'Usuarios listados com sucesso', usuarios })
+    }
+    catch (error) {
+        return res.status(500).json({
+            mensagem: 'Erro ao listar usuarios',
+            erro: error.mensagem
+        })
+    }
+
+
+}
 
--- rotas/rotas.js
+++ rotas/rotas.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, cadastrarUsuario, excluirUsuario, atualizarUsuario } from '../controller/cadastroUsuario.js';
+import { login, cadastrarUsuario, excluirUsuario, atualizarUsuario, visualizarUsuarios } from '../controller/cadastroUsuario.js';
 // import { autenticar } from '../middlewares/authMiddleware.js';
 import { cadastrarCarro, listarCarros, atualizarCarro, excluirCarro } from '../controller/cadastroCarro.js';
 import { registrarEntrada, registrarSaida } from '../controller/registro.js';
@@ -11,20 +11,21 @@ export const rotas = express.Router();
 // Rotas de usuários
 rotas.post('/usuarios/cadastrar', cadastrarUsuario);
 rotas.post('/usuarios/login', login);
+rotas.post('/usuarios/buscar', visualizarUsuarios)
 rotas.use(validarToken); // Middleware de autenticação aplicado globalmente
 
 rotas.put('/usuarios/atualizar/:id', atualizarUsuario);
 rotas.delete('/usuarios/excluir/:id', verificarAdmin, excluirUsuario); // Exclusão restrita a administradores
 
 // Rotas de veículos
-rotas.post('/veiculos/cadastrar', cadastrarCarro);
-rotas.get('/veiculos/listar', listarCarros);
-rotas.put('/veiculos/:id', atualizarCarro);
-rotas.delete('/veiculos/:id', excluirCarro);
+rotas.post('/veiculos/cadastrar',verificarAdmin, cadastrarCarro);
+rotas.get('/veiculos/listar',verificarAdmin, listarCarros);
+rotas.put('/veiculos/:id',verificarAdmin, atualizarCarro);
+rotas.delete('/veiculos/:id',verificarAdmin, excluirCarro);
 
 // Rotas de acessos
-rotas.post('/acessos/entrada', registrarEntrada);
-rotas.post('/acessos/saida', registrarSaida);
+rotas.post('/acessos/entrada',verificarAdmin, registrarEntrada);
+rotas.post('/acessos/saida',verificarAdmin, registrarSaida);
 
 // Rotas de vagas
 rotas.get('/vagas/disponiveis', visualizarVagasDisponiveis);
